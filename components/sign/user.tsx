80ee1fb067cbe57ceb4f7be6e5d2b9c207bae74e
EsonWang <2411396553@qq.com>
2025-04-18 18:10:30 +0800

    fix the bug at login

--- components/sign/user.tsx
+++ components/sign/user.tsx
@@ -40,12 +40,16 @@ export default function SignUser({ user }: { user: User }) {
         </DropdownMenuItem>
         <DropdownMenuSeparator />
 
-        <DropdownMenuItem className="flex justify-center cursor-pointer">
-          <Link href="/admin/users" target="_blank">
-            {t("user.admin_system")}
-          </Link>
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
+        {user.is_admin && (
+          <>
+            <DropdownMenuItem className="flex justify-center cursor-pointer">
+              <Link href="/admin/users" target="_blank">
+                {t("user.admin_system")}
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+          </>
+        )}
 
         <DropdownMenuItem
           className="flex justify-center cursor-pointer"
